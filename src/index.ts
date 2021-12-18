05a1617cbcfb5098c56b66af8ea1368a9267ec58
Vivek Sharma <32999354+viveksharma8130@users.noreply.github.com>
2021-12-18 19:58:45 +0530

    Update index.ts
    wqeqwe

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { Server } from "./server";
 
 let server = new Server().app;
-let host="192.168.1.104";
 
 let port = 3000;
 
-server.listen(port, host, () => {
+server.listen(port, () => {
     console.log(`EBUN : PORT ${port}`);
 });
 
