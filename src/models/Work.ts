3bb842f1e9bfacba717ab4994205dcd8c03e4e7e
viveksharma8130 <viveksharma8130866130@gmail.com>
2022-01-06 15:35:33 +0530

    sd

--- src/models/Work.ts
+++ src/models/Work.ts
@@ -4,6 +4,7 @@ import { Utils } from '../utils/Utils';
 
 const WorkSchema = new mongoose.Schema({
     media             : {type: String, required: true},
+    url             : {type: String, required: false},
     title             : {type: String, required: false},
     created_at        : {type: Date, required: true, default: Utils.indianTimeZone},
     updated_at        : {type: Date, required: true, default: Utils.indianTimeZone},
